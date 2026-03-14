26c6213e69f66b14c614722e153531136ef4119e
cyber nights <rupeshkk941@gmail.com>
2026-03-14 10:53:41 +0530

    Add files via upload

---
+++ multiplication.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+	int number1, number2, multiplication;
+	printf("Enter two intergers:");
+	scanf("%d %d",&number1, & number2); 
+	// calculate the sum 
+	multiplication = number1 * number2;
+	// caluclate the sum
+	
+	printf("%d * %d = %d", number1, number2, multiplication);
+	return 0;
+}
