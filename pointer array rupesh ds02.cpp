3bed46c0b3be1348dc7d7d6fee18111e85b8152e
cyber nights <rupeshkk941@gmail.com>
2026-05-02 10:33:07 +0530

    Add files via upload

---
+++ pointer array rupesh ds02.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int array[5] = {10,20,30,40,50};
+	int *ptr;
+	int i;
+	ptr = array;
+	printf("Accessing array elements using pointers:\n");
+	for(i=0;i<5; i++)
+	{
+	 printf("Element %d =%d\n",i, *(ptr +i )); 
+}   
+return 0;
+}
